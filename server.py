99594ab8fb59715d01a768b2b44d0a53f9c8f5af
nyksis <75268731+nyksis@users.noreply.github.com>
2022-04-08 09:18:41 +0300

    pievienojam bildes
    pievienojam bildes

--- server.py
+++ server.py
@@ -1,18 +1,44 @@
+from crypt import methods
 from flask import Flask
 from flask import request
 from flask import render_template
 
 app = Flask(__name__)
 
+def loadAllPictures():
+  filepath = 'picDB.txt' 
+  pictures=[]
+  with open (filepath) as fp:
+    line = fp.readline()
+    cnt = 1
+    while line:
+      if line!=' ':
+        fullPicInfo = line.strip().split(';')
+        pictures.append(fullPicInfo[1])
+      line = fp.readline()
+      cnt += 1
+  fp.close()
+  return pictures
+
 #Pirmā lapa, kas tiks ielādēta
 @app.route('/',methods = ['POST', 'GET'])
 def root():
     return render_template("index.html")
+
+@app.route('/about')
+def about ():
+  return render_template("about.html")
+
+@app.route('/test', methods = ['POST', 'GET'])
+def test():
+    parametri = ["IQ","Augums", "Māja"]
+    images = loadAllPictures()
+    return render_template("test.html", parametri = parametri, images=images)
     
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
 def health():
-  return "OK"
+  return "Viss kārtībā!"
 
 if __name__ == '__main__':
   app.run(debug="true")
